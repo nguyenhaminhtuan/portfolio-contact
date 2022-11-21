7aaab083a5f970b5843fcf0d4717b5e7874b60c3
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2022-11-21 19:35:46 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,46 @@ export interface Env {
 }
 
 const TELEGRAM_API_URL = 'https://api.telegram.org';
+
 const schema = z.object({
   from: z.string().email(),
   subject: z.string().min(2).max(200),
   message: z.string().min(2).max(4000),
 });
+
 type RequestBody = z.infer<typeof schema>;
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+function handleOptions(request: Request) {
+  let headers = request.headers;
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    let respHeaders = {
+      ...corsHeaders,
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers'
+      )!,
+    };
+
+    return new Response(null, {
+      headers: respHeaders,
+    });
+  } else {
+    return new Response(null, {
+      headers: {
+        Allow: 'POST, OPTIONS',
+      },
+    });
+  }
+}
 
 export default {
   async fetch(
@@ -19,38 +53,50 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const url = new URL(request.url);
-    const contentType = request.headers.get('content-type') ?? '';
-    if (url.pathname === '/' && request.method === 'POST') {
-      if (!contentType.includes('application/json') && !request.body) {
-        return new Response(null, { status: 400 });
-      }
-      const body: RequestBody = await request.json();
-      const validateResult = schema.safeParse(body);
-      if (!validateResult.success) {
-        return new Response(null, { status: 400 });
-      }
-
-      const { from, subject, message } = validateResult.data;
-      const query = new URLSearchParams({
-        chat_id: env.TELEGRAM_CHANNEL_ID,
-        text: `a new contact message from "${from}" with subject "${subject}" and message "${message}"`,
-      });
-      const reqUrl = `${TELEGRAM_API_URL}/bot${
-        env.TELEGRAM_BOT_TOKEN
-      }/sendMessage?${query.toString()}`;
-
-      const res = await fetch(reqUrl, {
-        method: 'GET',
-      });
-      const resData: { ok: boolean; result: any } = await res.json();
-
-      if (!resData || !resData.ok) {
-        return new Response(null, { status: 500 });
-      }
-
-      return new Response(null, { status: 200 });
+    if (request.method === 'OPTIONS') {
+      return handleOptions(request);
     }
-    return new Response(null, { status: 404 });
+
+    if (request.method !== 'POST') {
+      return new Response(null, {
+        status: 405,
+      });
+    }
+
+    const url = new URL(request.url);
+    if (url.pathname !== '/') {
+      return new Response(null, { status: 404 });
+    }
+
+    const contentType = request.headers.get('content-type') ?? '';
+    if (!contentType.includes('application/json') && !request.body) {
+      return new Response(null, { status: 400 });
+    }
+
+    const body: RequestBody = await request.json();
+    const validateResult = schema.safeParse(body);
+    if (!validateResult.success) {
+      return new Response(null, { status: 400 });
+    }
+
+    const { from, subject, message } = validateResult.data;
+    const query = new URLSearchParams({
+      chat_id: env.TELEGRAM_CHANNEL_ID,
+      text: `a new contact message from "${from}" with subject "${subject}" and message "${message}"`,
+    });
+    const reqUrl = `${TELEGRAM_API_URL}/bot${
+      env.TELEGRAM_BOT_TOKEN
+    }/sendMessage?${query.toString()}`;
+
+    const res = await fetch(reqUrl, {
+      method: 'GET',
+    });
+    const resData: { ok: boolean; result: any } = await res.json();
+
+    if (!resData || !resData.ok) {
+      return new Response(null, { status: 500 });
+    }
+
+    return new Response(null, { status: 200 });
   },
 };
